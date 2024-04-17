db339b737ae98a636f0afa45fbd1ab483307c510
Alex Rhinelander <arhinelander@armbrae.ns.ca>
2024-04-17 15:08:21 -0300

    Add files via upload

---
+++ ptclock/script.js
@@ -0,0 +1,23 @@
+const chime = new Audio("chime.mp3");
+
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  let dateStr = (hours % 12) || 12;
+  dateStr += ":" + minutes.toString().padStart(2, '0');
+  dateStr += ":" + seconds.toString().padStart(2, '0');
+  dateStr += hours >= 12 ? ' pm' : ' am';
+
+  document.getElementById('current-time-value').innerHTML = dateStr;
+
+  if (minutes % 10 === 0 && seconds % 60 === 0) {
+    chime.currentTime = 0;
+    chime.play();
+  }
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
