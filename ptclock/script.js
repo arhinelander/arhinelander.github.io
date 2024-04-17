5a064eeb3072b35417e14b137d61427a2b9c9520
Alex Rhinelander <arhinelander@armbrae.ns.ca>
2024-04-17 15:15:17 -0300

    Update script.js

--- ptclock/script.js
+++ ptclock/script.js
@@ -7,13 +7,12 @@ function updateTime() {
   const seconds = now.getSeconds();
   let dateStr = (hours % 12) || 12;
   dateStr += ":" + minutes.toString().padStart(2, '0');
-  //dateStr += ":" + seconds.toString().padStart(2, '0');
   dateStr += hours >= 12 ? ' pm' : ' am';
 
   document.getElementById('current-time-value').innerHTML = dateStr;
 
-  if (minutes % 1 === 0 && seconds % 10 === 0) {
-    chime.currentTime = 0;
+  if (minutes % 1 === 0 && seconds % 60 === 0) {
+    //chime.currentTime = 0;
     chime.play();
   }
 }
