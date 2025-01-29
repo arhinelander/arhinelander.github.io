f5cbcaf26225055e48824d1524ce3ade9f518a96
Alex Rhinelander <arhinelander@armbrae.ns.ca>
2025-01-29 14:52:11 -0400

    Update script.js

--- ptclock/script.js
+++ ptclock/script.js
@@ -11,7 +11,7 @@ function updateTime() {
   dateStr += hours >= 12 ? ' pm' : ' am';
   document.getElementById('current-time-value').innerHTML = dateStr;
 
-  if (minutes % 10 === 0 && seconds % 60 === 0) {
+  if (minutes % 2 === 0 && seconds % 60 === 0) {
     chime.play();
   }
 }
